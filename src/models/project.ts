9d3e15e3d87bc62a18a56f6577bdd213e8db6477
Micaiah Wallace <micaiah@snoapps.com>
2022-12-27 22:37:16 -0700

    remove unused fields in project

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,13 +1,10 @@
 import { JSONSchemaType } from 'ajv'
 import { validateSchema } from '@/utils/validate-schema'
 
-const MAX_EVENTS_PER_PROJECT = 250
-
 export interface IProject {
   __type: 'project'
   id: string
   name: string
-  events: Array<string>
   organization_id: string
   updated_at: string
 }
@@ -18,17 +15,10 @@ const schema: JSONSchemaType<IProject> = {
     __type: { type: 'string', const: 'project' },
     id: { type: 'string' },
     name: { type: 'string' },
-    events: {
-      type: 'array',
-      uniqueItems: true,
-      items: { type: 'string' },
-      minItems: 0,
-      maxItems: MAX_EVENTS_PER_PROJECT,
-    },
     organization_id: { type: 'string' },
     updated_at: { type: 'string' },
   },
-  required: ['__type', 'id', 'name', 'events', 'organization_id', 'updated_at'],
+  required: ['__type', 'id', 'name', 'organization_id', 'updated_at'],
 }
 
 export const isProject = validateSchema<IProject>(schema)
